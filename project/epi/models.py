585d70caa060de71c3b5eb5800135f5d49ee30e5
Jay Buie <john.buie@cgifederal.com>
2018-03-26 10:39:00 -0400

    SourceTree is working.  I removed the comment.

--- project/epi/models.py
+++ project/epi/models.py
@@ -19,8 +19,6 @@ from utils.helper import SerializerHelper, HAWCDjangoJSONEncoder
 
 from . import managers
 
-# This is a test comment to see if my SourceTree connection to GitHub is working now.
-
 class Criteria(models.Model):
     objects = managers.CriteriaManager()
 
