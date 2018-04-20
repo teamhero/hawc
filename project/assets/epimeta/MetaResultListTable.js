458b5f930b713ea3abb60aedf6f65c9d58aa6b90
wschwab3 <walter.schwab@cgifederal.com>
2018-04-20 15:28:01 -0400

    added cvlickable headers for sorting to all endpoint lists

--- project/assets/epimeta/MetaResultListTable.js
+++ project/assets/epimeta/MetaResultListTable.js
@@ -13,16 +13,21 @@ class MetaResultListTable {
             return '<p>No results available.</p>';
         }
 
-        var table = this.table,
-            headers = [
-                'Study',
-                'Meta result',
-                'Protocol',
-                'Health outcome',
-                'Exposure',
-                'Confidence interval',
-                'Estimate',
-            ];
+        var x, table = this.table,
+            headerTexts = [
+			    {text:'Study',link:'protocol__study__short_citation'},
+	            {text:'Meta result',link:'label'},
+                {text:'Protocol',link:'protocol__name'},
+                {text:'Health outcome',link:'health_outcome'},
+                {text:'Exposure',link:'exposure'},
+                {text:'Confidence interval',link:'lower_ci'},
+                {text:'Estimate',link:'estimate'},
+            ],
+			headers = [];
+		for (x in headerTexts) 
+		    headers.push($('<a href="'+location.origin+location.pathname+'?order_by={0}">'.printf(headerTexts[x].link)).html(headerTexts[x].text));
+        headers.pop();
+
         table.setColGroup([10, 16, 19, 12, 11, 10, 10]);
         table.addHeaderRow(headers);
         this.endpoints.map((endpoint) => {
