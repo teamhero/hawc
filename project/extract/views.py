82824a9dd6a91ed475c1bfd51edf927b510abbca
abennett2 <adam.bennett@cgifederal.com>
2018-05-25 15:17:14 -0400

    Code to add HERO tag tree to HAWC database

--- project/extract/views.py
+++ project/extract/views.py
@@ -14,6 +14,7 @@ import requests
 import json
 import os.path
 import re
+import collections
 from assessment.models import Attachment, Assessment
 from utils.models import get_crumbs
 from lit.models import Reference, Search, ReferenceFilterTag
@@ -48,6 +49,22 @@ class Home(TemplateView):
     def get(*args, **kwargs):
         return HttpResponse("HERO Extracted Code will go here.")
 
+# class TagByUntagged(TagReferences):
+#     """
+#     View to tag all untagged references for an assessment.
+#     """
+#     model = Assessment
+
+#     def get_assessment(self, request, *args, **kwargs):
+#         self.object = get_object_or_404(Assessment, id=self.kwargs.get('pk'))
+#         return self.object
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['references'] = models.Reference.objects.get_untagged_references(self.assessment)
+#         context['tags'] = models.ReferenceFilterTag.get_all_tags(self.assessment.id)
+#         return context
+
 class HeroAdd(TemplateView):
     template_name = 'extract/heroadd.html'
     heroURL = "http://localhost/hero/index.cfm/api/1.0/referencetagger/getprojecttagtree"
@@ -74,6 +91,15 @@ class HeroAdd(TemplateView):
 
     def post(self, request, *args, **kwargs):
         assessment_id = self.kwargs.get('pk')
+
+        # Emergency rebuild of default Tree
+        #assessment = Assessment.objects.filter(pk = assessment_id)
+        #print(assessment)
+        #ReferenceFilterTag.build_default(assessment[0])
+
+        tempContext = super().get_context_data(**kwargs)
+        references = Reference.objects.get_untagged_references(assessment_id)
+
         project_id = request.POST.get('heroproject')
         thisObject = self.get_object()
         response = requests.post(
@@ -84,97 +110,58 @@ class HeroAdd(TemplateView):
                 ,"Content-Type": "application/json"
             }
         )
-        #print(request.POST.get('heroproject'))
         print(self.kwargs.get('pk'))
         myVar = response.text
         tempVar = json.loads(myVar)
         myVar2 = tempVar["methodResult"]["tagTree"]
-        tempString = ''
-        count1 = 0
+        newNode = {}
         curLevel = 0
-        if len(myVar2["children"]) > 0:
-            tempString = tempString + ', {"data": {"name": "HERO Tags", "slug": "hero-tags"}, "id": ' + project_id + ', "children": ['
+        #cache.clear()
+        tbtags = ReferenceFilterTag.get_assessment_root(self.kwargs.get('pk'))
+
+        testHero = tbtags.get_children()
+
+        if testHero.filter(name="HERO Tags"):
+            curLevel = 2
+        else:
+            if len(myVar2["children"]) > 0:
+                newNode['data'] = collections.OrderedDict()
+
+            # The variable 'jtags' Grabs the default Tag Tree
+            jtags = ReferenceFilterTag.get_all_tags(self.kwargs.get('pk'), False)
+            print("In here!")
+            print(curLevel)
+            tags = tbtags.add_child(name = 'HERO Tags')
+
+            node = {}
             for key in myVar2["children"]:
                 curLevel = 1
-                #print(key["usage"])
                 usage1 = str(key["usage"])
                 usageID1 = str(key["usage_id"])
-                level1 = str(key["level"] + 1)
-                slug1 =  re.sub(" ", "-", usage1, 0, re.MULTILINE).lower()
-                tempString = tempString + '{"data": {"name": "' + usage1 + '", "slug": "' + slug1 + '"}, "id": ' + usageID1 + '}'
+                node[usageID1] = tags.add_child(name = usage1)
+
                 if len(key["children"]) > 0:
-                    newCount1 = len(tempString) - 1
-                    tempString = tempString[:newCount1]
-                    tempString = tempString + ', "children": ['
                     for key2 in key["children"]:
-                        curLevel = 2
-                        print(key2["usage"])
                         usage2 = str(key2["usage"])
                         usageID2 = str(key2["usage_id"])
-                        level2 = str(key2["level"] + 1)
-                        slug2 =  re.sub(" ", "-", usage2, 0, re.MULTILINE).lower()
-                        tempString = tempString + '{"data": {"name": "' + usage2 + '", "slug": "' + slug2 + '"}, "id": ' + usageID2 + '}'
+                        node[usageID2] = node[usageID1].add_child(name = usage2)
+
                         if len(key2["children"]) > 0:
-                            newCount2 = len(tempString) - 1
-                            tempString = tempString[:newCount2]
-                            tempString = tempString + ', "children": ['
                             for key3 in key2["children"]:
-                                curLevel = 3
-                                print(key3["usage"])
                                 usage3 = str(key3["usage"])
                                 usageID3 = str(key3["usage_id"])
-                                level3 = str(key3["level"] + 1)
-                                slug3 =  re.sub(" ", "-", usage3, 0, re.MULTILINE).lower()
-                                tempString = tempString + '{"data": {"name": "' + usage3 + '", "slug": "' + slug3 + '"}, "id": ' + usageID3 + '}'
+                                node[usageID3] = node[usageID2].add_child(name = usage3)
+
                                 if len(key3["children"]) > 0:
-                                    newCount3 = len(tempString) - 1
-                                    tempString = tempString[:newCount3]
-                                    tempString = tempString + ', "children": ['
                                     for key4 in key3["children"]:
-                                        curLevel = 4
-                                        print(key4["usage"])
                                         usage4 = str(key4["usage"])
                                         usageID4 = str(key4["usage_id"])
-                                        level4 = str(key4["level"] + 1)
-                                        slug4 =  re.sub(" ", "-", usage4, 0, re.MULTILINE).lower()
-                                        tempString = tempString + '{"data": {"name": "' + usage4 + '", "slug": "' + slug4 + '"}, "id": ' + usageID4 + '}'
-                                        newCount = len(tempString) - 3
-                                        tempString = tempString[:newCount]
-                                        tempString = tempString + '}]}, '
-                                else:
-                                    tempString = tempString + ', '
-                            newCount = len(tempString) - 3
-                            tempString = tempString[:newCount]
-                            tempString = tempString + '}]}, '
-                        else:
-                            tempString = tempString + ', '
-                    
-                    newCount = len(tempString) - 3
-                    tempString = tempString[:newCount]
-                    tempString = tempString + '}]}, '
-                else:
-                    tempString = tempString + ', '
-            tempString = tempString + '}]}, '
+                                        node[usageID4] = node[usageID3].add_child(name = usage4)
 
-        # Back out to the top of the JSON (curLevel is how many levels back up we need to climb)
+        ftags = ReferenceFilterTag.get_all_tags(self.kwargs.get('pk'), False)
+        finalTags = json.dumps(ftags)
 
-        #backOut = len(tempString) - (((curLevel - 1) * 5) + 3)
-        backOut = len(tempString) - 8
-        tempString2 = tempString[:backOut] + '}]'
-
-        # print(tempString2)
-        print(curLevel)
-
-        # The variable 'tags' Grabs the default Tag Tree
-        tags = ReferenceFilterTag.get_all_tags(self.kwargs.get('pk'))
-
-        # Here we add the HERO tags into the generated HAWC tags with the variable 'newTags'
-        newCount = len(tags) - 3
-        newTags = (tags[:newCount] + tempString2 + '}]}]')
-
-        # Here we send everything we need to the view (Swap lines for testing)
-        #context = {'project_id': project_id, 'object': thisObject, 'assessment_id': self.kwargs.get('pk'), 'myVar': myVar, 'myVar2': myVar2, 'tags': newTags, 'tempString': tags, 'tempStringA': tempString2, 'tempStringB': tempString, 'curLevel': curLevel,}
-        context = {'project_id': project_id, 'object': thisObject, 'assessment_id': self.kwargs.get('pk'), 'myVar2': myVar2, 'tags': newTags, 'curLevel': curLevel,}
+        context = {'project_id': project_id, 'object': thisObject, 'assessment_id': self.kwargs.get('pk'), 'myVar2': myVar2, 'tags': finalTags, 'curLevel': curLevel, 'references': references,}
         return render(request, self.template_name, context)
 
 class Hero(TemplateView):
