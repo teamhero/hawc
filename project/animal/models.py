0e1c8cbb5cbdf8386bd0b93db565f7f6b47d64b9
Shane Thacker <sst@clockworkmind.com>
2018-07-25 14:43:11 -0400

    Changed instructions for experiments.

--- project/animal/models.py
+++ project/animal/models.py
@@ -115,7 +115,14 @@ class Experiment(models.Model):
                     "described in a non-standard way)",
         blank=True)
     diet = models.TextField(
-        help_text="Description of animal-feed (use diet as described in methods)",
+        help_text="Copy paste diet/water from materials and methods, "
+                    "use quotation marks around all text directly copy/pasted "
+                    "from paper. If diet is of particular importance in an "
+                    "assessment, then use a short description so it can be "
+                    "displayed in visualizations (e.g., soy-protein free 2020X "
+                    "Teklad). In these cases, the longer materials and methods "
+                    "description can be captured in 'animal husbandry' in the "
+                    "Animal Group extraction module.",
         blank=True)
     guideline_compliance = models.CharField(
         max_length=128,
