87f36e3d8247a61944103c0d0a5215249ec14660
Jay Buie <john.buie@cgifederal.com>
2018-10-12 12:45:18 -0400

    This commit completes this ticket.

--- project/utils/templatetags/keyvalue.py
+++ project/utils/templatetags/keyvalue.py
@@ -3,6 +3,7 @@ from django import template
 register = template.Library()
 
 
+# This fuction attempts to return the associated value for the specified key
 @register.filter
-def keyvalue(dict, value):
-	return dict[value] if (value in dict) else ""
+def keyvalue(dict, key):
+	return dict[key] if (key in dict) else ""
